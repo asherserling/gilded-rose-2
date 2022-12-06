5a9ce89bf44de3e7dbca9c9bcc56aaeda45999d6
Asher Serling <asher.s@shipboss.io>
2022-12-06 14:05:58 +0200

    Add validation to settings form

--- src/cljs/gilded_rose_2/pages/settings.cljs
+++ src/cljs/gilded_rose_2/pages/settings.cljs
@@ -4,52 +4,60 @@
             [gilded-rose-2.notifications :as notifications]
             [gilded-rose-2.settings :as s]))
 
-(declare rate-field)
+(declare input-number validate-state)
 
 (defn settings []
-  (let [initial-state @(rf/subscribe [::s/settings])
-        form-state (r/atom initial-state)]
-    [:div.columns
-     [:div.column.is-one-third
-      [notifications/notifications]]
-     [:div.column.is-one-third
-      [:div.card
-       [:div.card-header
-        [:div.card-header-title "Settings"]
-        [:div.card-header-icon
-         [:span.icon.fas.fa-gear]]]
-       [:div.card.card-content
-        [:<>
-         [rate-field "Supplier Rate" :supplier-rate form-state]
-         [rate-field "Seller Rate" :seller-rate form-state]]
-        
-        [:div.is-flex.is-justify-content-end.pt-3
-         [:button.button.is-info
-          {:on-click (fn [] (when (not= @form-state initial-state)
-                             (do
-                              (doseq [[key val] @form-state]
-                                (rf/dispatch [::s/set-key key val]))
-                              (rf/dispatch [::notifications/add-notification :success "Settings updated"]))))}
-          "Save"]]]]]]))
+  [:div.columns
+   
+   [:div.column.is-one-third 
+    [notifications/notifications]]
+   
+   [:div.column.is-one-third
+    [:div.card
+     [:div.card-header
+      [:div.card-header-title "Settings"]
+      [:div.card-header-icon
+       [:span.icon.fas.fa-gear]]]
+     [:div.card.card-content
+      (let [initial-state @(rf/subscribe [::s/settings])
+            form-state (r/atom initial-state)
+            errors (r/atom (validate-state initial-state))]
+        [:form
+         [:<>
+          [input-number "Seller Rate" :seller-rate form-state errors]
+          [input-number "Supplier Rate" :supplier-rate form-state errors]]
 
-(defn rate-field [label key form-state]
+         [:div.is-flex.is-justify-content-end.pt-3
+          [:button.button.is-info
+           {:on-click (fn [e]
+                        (when (and  (not= initial-state @form-state) (= 0 (count @errors)))
+                          (.preventDefault e)
+                          (doseq [[key val] @form-state]
+                            (rf/dispatch [::s/set-key key val]))
+                          (rf/dispatch [::notifications/add-notification :success "Settings updated"])))}
+           "Save"]]])]]
+
+    [:div.box.mt-2]]])
+
+(defn input-number [label key form-state errors]
   [:div.field
-   [:label.label label]
+   [:div.is-flex.is-flex-row.is-justify-content-space-between
+    [:label.label label]
+    (when (key @errors) [:div.tag.is-danger.is-light (key @errors)])]
    [:div.control
     [:input.input
      {:type "number"
+      :required true
       :value (key @form-state)
-      :on-change #(swap! form-state assoc key (-> % .-target .-value))}]]])
+      :on-change (fn [e]
+                   (.preventDefault e)
+                   (swap! form-state assoc key (-> e .-target .-value))
+                   (reset! errors (validate-state @form-state)))}]]])
 
-
-(comment
-  (rf/dispatch [::s/set-key :supplier-rate 1])
-  (s/get-key :supplier-rate)
-  temp
-  
-  (fn [] (for [[key val] @form-state]
-           (do
-             (.log js/console [key val])
-             (rf/dispatch [::s/set-key key val])))) 
-  
-  )
+(defn validate-state [form-state]
+  (cond-> {}
+    (<= (:supplier-rate form-state) 0) 
+    (assoc :supplier-rate "Supplier rate must be greater than zero")
+    
+    (<= (:seller-rate   form-state) 0)
+    (assoc :seller-rate   "Seller rate must be greater than zero")))
